000cbfd293da0a22607499331928e4c6b16a86d0
Samuel Teich <steich@gmail.com>
2017-10-07 13:16:16 -0500

    refactored motor and servo stuff

--- car_api.py
+++ car_api.py
@@ -1,12 +1,17 @@
 from flask import Flask 
 from flask_restful import Resource, Api
+from car import Motor, Servo
 
 app = Flask(__name__)
 api = Api(app)
+motor = Motor()
+servo = Servo()
 
 class Move(Resource):
 	def get(self):
 		#do something here
+		motor.go(some content)
+		servo.go(some content)
 		return {'success':'True'}
 
 api.add_resource(Move, '/')
