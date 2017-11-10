07b2e6b1a35266eec9fb1d3aa8305b98eda7fb33
Samuel Teich <steich@gmail.com>
2017-11-09 20:39:18 -0600

    starting in on sending images

--- car_api.py
+++ car_api.py
@@ -1,6 +1,12 @@
-from flask import Flask, request 
+from flask import Flask, request
+from flask import send_file
 from flask_restful import Resource, Api, reqparse
 from car import Motors, Servo
+import re
+import cv2
+import subprocess
+from PIL import Image
+from StringIO import StringIO
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +15,11 @@ servo = Servo()
 parser = reqparse.RequestParser()
 parser.add_argument('speed', default=None)
 parser.add_argument('angle', default=None)
+
+#from theopolime/webcam-streamer
+def list_camera_ids():
+    cameras = subprocess.Popen(['ls', '/dev/video*'], stdout=subprocesses.PIPE).comunicate()[0]
+    return re.findall(r'\d+', cameras)
 
 ACTION = {
     'speed':None,
@@ -36,7 +47,22 @@ class Move(Resource):
             motors.set_speed(ACTION['speed'])
         if ACTION['angle'] != None:
             servo.set_angle(ACTION['angle'])
-            
+
+class Picture(Resource):
+    
+    def __init__(self, *args, **kwargs):
+        self.cam = cv2.VideoCapture(int(list_camera_ids()[0]))
+        self.super().__init(*args, **kwargs)
+
+    def get(self):
+        ret, frame = self.cam.read()
+        image = Image.fromarray(frame)
+        buff = StringIO()
+        image.save(buff, 'JPEG')
+        _, img_encoded = cv2.imencode('JPEG', buff.getvalue())
+        
+
+
 api.add_resource(Move, '/Move')
 
 if __name__ == '__main__':
